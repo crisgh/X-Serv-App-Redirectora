ea09d5230b104a7524c6a696ec9ab8b137fbb47f
Cristina Gallego <cristina@ubuntu>
2016-03-09 13:42:33 +0100

    completo

--- redirectora.py
+++ redirectora.py
@@ -8,6 +8,6 @@ class aleat(webapp.webApp):
     def process(self,parsedRequest):
         numero = random.randint(1 , 99999)
         return("301 Moved Permanently" , "\r\n"
-                + "<html><body><h1><a href = '" +str(numero)+"'>Dame otra</h1></body></hmtl>")
+                + "<html><body><h1><a href = '" +str(numero)+"'>Dame otra</h1></body></hmtl>"+"\r\n")
 if __name__ == "__main__":
     testWebApp = aleat("localhost",2312)
